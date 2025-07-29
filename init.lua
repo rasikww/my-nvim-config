6fe610d0f069e645508eea02f9081b5645bc5a8d
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2025-07-29 17:26:16 +0530

    add bufferline desc

--- init.lua
+++ init.lua
@@ -420,6 +420,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>b', group = '[B]ufferline', mode = { 'n' } },
       },
     },
   },
