e16b9cb4da8335d8e32a9d76b0b9e831d6192cd5
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2025-11-09 03:06:43 +0530

    remove schedule to apply clipboard and just apply it directly

--- init.lua
+++ init.lua
@@ -27,9 +27,10 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+-- vim.schedule(function()
+--   vim.o.clipboard = 'unnamedplus'
+-- end)
+vim.opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
