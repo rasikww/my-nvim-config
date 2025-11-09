ed71daa3b2d7b15775b8211f9738cbe7a434588b
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2025-11-09 17:35:56 +0530

    add catgooses colorizer fork
    to get oklch

--- lua/custom/plugins/colorizer.lua
+++ lua/custom/plugins/colorizer.lua
@@ -1,6 +1,23 @@
 return {
-  'norcalli/nvim-colorizer.lua',
+  'catgoose/nvim-colorizer.lua',
   config = function()
-    require('colorizer').setup()
+    require('colorizer').setup {
+      user_default_options = {
+        oklch_fn = true,
+        RGB = true,
+        RRGGBB = true,
+        names = true,
+        RRGGBBAA = false,
+        rgb_fn = false,
+        hsl_fn = true,
+        css = false,
+        css_fn = false,
+        mode = 'background',
+        tailwind = true,
+        tailwind_opts = { -- Options for highlighting tailwind names
+          update_names = true, -- When using tailwind = 'both', update tailwind names from LSP results.  See tailwind section
+        },
+      },
+    }
   end,
 }
