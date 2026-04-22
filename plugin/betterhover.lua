043f080c1c64f0ccc864c9985111d11841bf487e
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2026-04-22 15:24:24 +0530

    feat(better-hover): add astro to better hover and a toggle keymap to disable and enable it

--- plugin/betterhover.lua
+++ plugin/betterhover.lua
@@ -1,6 +1,7 @@
+local hover_enabled = true
 vim.api.nvim_create_autocmd("FileType", {
 	once = true,
-	pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact", "tailwindcss", "svelte" },
+	pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact", "tailwindcss", "svelte", "astro" },
 	callback = function()
 		vim.pack.add({
 			{ src = "https://github.com/Sebastian-Nielsen/better-type-hover" },
@@ -11,3 +12,21 @@ vim.api.nvim_create_autocmd("FileType", {
 		})
 	end,
 })
+
+vim.keymap.set("n", "<leader>tj", function()
+	hover_enabled = not hover_enabled
+
+	if hover_enabled then
+		-- Enable Better Hover on Shift+J
+		vim.keymap.set("n", "<S-j>", function()
+			require("better-type-hover").open_type_doc()
+		end, { desc = "Better Hover" })
+
+		print("Better Hover ENABLED")
+	else
+		-- Restore default J behavior
+		vim.keymap.set("n", "<S-j>", "J", { desc = "Default J" })
+
+		print("Better Hover DISABLED")
+	end
+end, { desc = "[T]oggle Better Hover" })
