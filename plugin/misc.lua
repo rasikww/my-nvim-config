e5acc4417e1b69f20303bfc40670a50ec22baac6
Rasika Premachandra <130959620+rasikww@users.noreply.github.com>
2026-06-26 18:26:37 +0530

    feat: add comment enhaciing pluggin to misc file

--- plugin/misc.lua
+++ plugin/misc.lua
@@ -2,6 +2,8 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
 	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim", name = "ibl" },
 	{ src = "https://github.com/folke/todo-comments.nvim" },
+	{ src = "https://github.com/JoosepAlviste/nvim-ts-context-commentstring" },
+	{ src = "https://github.com/numToStr/Comment.nvim" },
 })
 
 require("ibl").setup({})
@@ -16,3 +18,7 @@ require("todo-comments").setup({})
 --TEST:
 
 require("vim._core.ui2").enable()
+
+require("Comment").setup({
+	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+})
